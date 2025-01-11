e9a52b7e9ccb3df4290c05d82d3ea9d46b4e7692
Xharry01X <harshitsingh50621@gmail.com>
2025-01-12 00:59:20 +0530

    printing star

--- questions/q9/main.rs
+++ questions/q9/main.rs
@@ -7,7 +7,7 @@ fn main(){
 fn print_star(n:i32){
 
   for i in 1..=n {
-    for _ in 0..i {
+    for _ in 0..i {   // The underscore is used as a placeholder for the loop variable when the variable itself isn't needed inside the loop.
         print!("*");
     }
     println!();
